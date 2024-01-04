cfed1940bf612398e215b50e68f1557b14aab0d5
r0meia <147402809+r0meia@users.noreply.github.com>
2024-01-04 19:42:27 +0100

    script

--- script.js
+++ script.js
@@ -40,10 +40,80 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
 
 
-}
+};
 function showPreviousSlides() {
+    let i;
+ 
+    let slides = document.getElementsByClassName("sneakers");
+ 
+ 
+    for (i = 0; i < slides.length; i++) {
 
+        slides[i].style.display = "none";
+    }
+ 
+    slideIndex--;
+ 
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+ 
+    slides[slideIndex - 1].style.display = "block";
+
+
+};
     
-}
-arrowLeft.addEventListener("click", (showSlides));
+
+arrowLeft.addEventListener("click", (showPreviousSlides));
 arrowRight.addEventListener("click", (showSlides));
+
+const cartButton = document.querySelector(".nav-cart");
+const amount = document.querySelector(".number-amount");
+const cartEmpty = document.querySelector(".cart-wrapper-wrapper");
+const minus = document.querySelector(".icon-minus");
+const plus = document.querySelector(".icon-plus");
+let itemsNumber = document.querySelector(".number-amount");
+
+cartButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    
+    cartEmpty.classList.add("active");
+
+    document.addEventListener("click", () => {
+        cartEmpty.classList.remove("active");
+    });
+});
+
+cartEmpty.addEventListener("click", (event) => {
+    event.stopPropagation();
+});
+
+function changeNumber(numberAmount) {
+
+    const currentNumber = parseInt(itemsNumber.textContent.match(/\d+/)[0]);
+
+    const newNumber = currentNumber + numberAmount;
+    itemsNumber.textContent = newNumber;
+
+}
+
+plus.addEventListener("click", () => {
+    changeNumber(1);
+});
+
+function subtractNumber(numberAmount) {
+
+    let currentNumber = parseInt(itemsNumber.textContent.match(/\d+/)[0]);
+
+    if(currentNumber > 0) {
+        let newNumber = currentNumber - numberAmount;
+        itemsNumber.textContent = newNumber;
+    } else {
+        itemsNumber.textContent = 0;
+    }
+
+}
+minus.addEventListener("click", () => {
+    subtractNumber(1);
+});
+
